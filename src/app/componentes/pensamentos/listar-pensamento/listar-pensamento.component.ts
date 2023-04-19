9df558c5ecf7d4be82f0230b350f885f0f4b78d7
BragaMPedro <pedrobmagalhaes95@gmail.com>
2023-04-19 20:08:33 -0300

    barra de busca e favoritos

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,44 +1,72 @@
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento/pensamento';
 import { PensamentoService } from '../pensamento.service';
+import { ActivatedRoute, Router, provideRouter } from '@angular/router';
 
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.css']
+  styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
-
   listaPensamentos: Pensamento[] = [];
   haMaisPensamentos: boolean = true;
   paginaAtual: number = 1;
-  filtro: string = "";
+  filtro: string = '';
+  favoritos: boolean = false;
+  listaFavoritos: Pensamento[] = [];
 
-  constructor(private service: PensamentoService) {}
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit():void {
-    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) =>{
-      this.listaPensamentos = listaPensamentos;
-    });
-  }
-
-  carregarMaisPensamentos() {
-    this.service.listar(++this.paginaAtual, this.filtro)
-      .subscribe(listaPensamentos => {
-        this.listaPensamentos.push(...listaPensamentos);
-        if(!listaPensamentos.length) {
-          this.haMaisPensamentos = false;
-        };
+  ngOnInit(): void {
+    this.service
+      .listar(this.paginaAtual, this.filtro, this.favoritos)
+      .subscribe((listaPensamentos) => {
+        this.listaPensamentos = listaPensamentos;
       });
   }
 
-  pesquisarPensamentos(){
-    this.paginaAtual = 1
-    this.haMaisPensamentos = true;
-    this.service.listar(this.paginaAtual, this.filtro)
-      .subscribe(listaPensamentos => {
-        this.listaPensamentos = listaPensamentos
-      })
+  carregarMaisPensamentos() {
+    this.service
+      .listar(++this.paginaAtual, this.filtro, this.favoritos)
+      .subscribe((listaPensamentos) => {
+        this.listaPensamentos.push(...listaPensamentos);
+        if (!listaPensamentos.length) {
+          this.haMaisPensamentos = false;
+        }
+      });
   }
 
+  pesquisarPensamentos() {
+    this.paginaAtual = 1;
+    this.haMaisPensamentos = true;
+    this.service
+      .listar(this.paginaAtual, this.filtro, this.favoritos)
+      .subscribe((listaPensamentos) => {
+        this.listaPensamentos = listaPensamentos;
+      });
+  }
+
+  listarFavoritos() {
+    this.favoritos = true;
+    this.haMaisPensamentos = true;
+    this.paginaAtual = 1;
+    this.service
+      .listar(this.paginaAtual, this.filtro, this.favoritos)
+      .subscribe((listaPensamentosFavoritos) => {
+        this.listaPensamentos = listaPensamentosFavoritos;
+        this.listaFavoritos = listaPensamentosFavoritos;
+      });
+  }
+
+  recarregarComponente() {
+    this.favoritos = false;
+    this.paginaAtual = 1;
+
+    this.router.navigate([this.router.url], { onSameUrlNavigation: 'reload' });
+  }
 }
--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -4,37 +4,43 @@ import { Pensamento } from './pensamento/pensamento';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PensamentoService {
+  private readonly API = 'http://localhost:3000/pensamentos';
 
-  private readonly API = "http://localhost:3000/pensamentos"
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-    
-  listar(pagina: number, filtro: string): Observable<Pensamento[]> {
-    
+  listar(
+    pagina: number,
+    filtro: string,
+    favoritos: boolean
+  ): Observable<Pensamento[]> {
     //Paginação JsonServer:
     //GET /posts?_page7&_limit-15
     const itensPorPagina = 6;
     let params = new HttpParams()
-    .set("_page", pagina)
-    .set("_limit", itensPorPagina);
+      .set('_page', pagina)
+      .set('_limit', itensPorPagina);
 
-    if(filtro.trim().length > 2){
-      params = params.set("q", filtro)
+    if (filtro.trim().length > 2) {
+      params = params.set('q', filtro);
     }
 
-    return this.http.get<Pensamento[]>(this.API,{ params })
+    if (favoritos) {
+      params = params.set('favorito', true);
+    }
+
+    return this.http.get<Pensamento[]>(this.API, { params });
   }
 
-  buscarPorId(id: number): Observable<Pensamento>{
+  buscarPorId(id: number): Observable<Pensamento> {
     const url = `${this.API}/${id}`;
 
     return this.http.get<Pensamento>(url);
   }
 
-  criar(pensamento: Pensamento): Observable<Pensamento>{
+  criar(pensamento: Pensamento): Observable<Pensamento> {
     return this.http.post<Pensamento>(this.API, pensamento);
   }
 
@@ -45,15 +51,14 @@ export class PensamentoService {
   }
 
   mudarFavorito(pensamento: Pensamento): Observable<Pensamento> {
-    pensamento.favorito = !pensamento.favorito
+    pensamento.favorito = !pensamento.favorito;
 
     return this.editar(pensamento);
   }
 
-  excluir(id: number): Observable<Pensamento>{
+  excluir(id: number): Observable<Pensamento> {
     const url = `${this.API}/${id}`;
 
     return this.http.delete<Pensamento>(url);
   }
-
 }
